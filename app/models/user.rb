0b8656d3392cd2741ba5d9ef65e4f61c2f8d51bb
Apprentice <gin3002@gmail.com>
2015-07-31 11:11:35 +1200

    add variable navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,16 @@ class User < ActiveRecord::Base
   has_many :follow_relationships
   has_many :followers, through: :follow_relationships, :foreign_key => 'follower_id'
 
-include BCrypt
+# include BCrypt
 
-  def password_check
-    @password ||= Password.new(password)
-  end
+#   def password_check
+#     @password ||= Password.new(password)
+#   end
 
-  def password=(new_password)
-    @password =Password.create(new_password)
-    self.password = @password
-  end
+#   def password=(new_password)
+#     @password =Password.create(new_password)
+#     self.password = @password
+#   end
 end # End of User classbundke
 
 
