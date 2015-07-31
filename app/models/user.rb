0848b6a404b976d2dab11409c4c302ee0d3ecd06
IonaCS <apprentice@StMac8.local>
2015-07-31 14:55:01 +1200

    add unique user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   has_many :bleets
   has_many :follow_relationships
   has_many :followers, through: :follow_relationships, :foreign_key => 'follower_id'
+  validates :email, uniqueness: true
+
 
  include BCrypt
 
